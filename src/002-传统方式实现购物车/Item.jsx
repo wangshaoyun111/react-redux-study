0fa3391110de7ac90298a03a9abc587f05ac0197
wsy <18611031643.163.com>
2021-04-27 14:20:39 +0800

    初始化数据并控制商品的加与减

--- src/002-传统方式实现购物车/Item.jsx
+++ src/002-传统方式实现购物车/Item.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
 
 class Item extends React.Component {
+  state = {
+    count: 0
+  }
+  // 购买数量加1
+  handleAdd = () => {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+  // 购买数量减1
+  handleSub = () => {
+    if (this.state.count <= 0) return
+    this.setState({
+      count: this.state.count - 1
+    })
+  }
   render() {
+    const { count } = this.state
+    const { name } = this.props
     return (
       <div className="item-container">
-        <span>【xxx】商品的购买数量是：【0】</span>
-        <button>+</button>
-        <button>-</button>
+        <span>【{name}】商品的购买数量是：【{count}】</span>
+        <button onClick={this.handleAdd}>+</button>
+        <button onClick={this.handleSub}>-</button>
       </div>
     )
   }
